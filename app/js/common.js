68b4c44cc61c1d8cd0e0557829750b669e64bc25
Vonasur <rusanov@vonasur.ru>
2018-10-02 22:26:32 +0200

    Digitalthinx

---
+++ app/js/common.js
@@ -0,0 +1,28 @@
+		$(document).ready(function () {
+			$('#slider1').tinycarousel();
+		});
+
+
+
+		(function($){
+			$(window).on("load",function(){
+				
+				/* Page Scroll to id fn call */
+				$("#navigation-menu a,a[href='#top'],a[rel='m_PageScroll2id']").mPageScroll2id({
+					highlightSelector:"#navigation-menu a"
+				});
+				
+				/* demo functions */
+				$("a[rel='next']").click(function(e){
+					e.preventDefault();
+					var to=$(this).parent().parent("section").next().attr("id");
+					$.mPageScroll2id("scrollTo",to);
+				});
+				
+			});
+
+			
+		})(jQuery);
+
+			
+		new WOW().init();
---
+++ readme.md
@@ -0,0 +1,7 @@
+<h1>Digitalthinx</h1>
+
+<p>
+	Landing page консалтинговой компании.
+</p>
+
+<p>Bootstrap 3, JQuery, Animate.css, Tinycarousel, Scroll2id, Sass, Gulp</p>
